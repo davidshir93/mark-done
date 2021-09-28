0b3b2363451e27cf26328b7320c225b3a127bca3
David Shir <davidshir93@gmail.com>
2021-09-28 14:43:31 +0300

    slider design

--- src/Components/Goal/Goal.js
+++ src/Components/Goal/Goal.js
@@ -4,18 +4,20 @@ import "./Goal.css";
 const Goal = ({ name, done, goalValue, measuring, id, handleDelete, handleSlideChange }) => {
   
   return (
-    <div className="goal">
+    <div className={`goal ${ done - goalValue >= 0 ? 'completed' : ''}`}>
         <input type="range" min='0' value={done} max={goalValue} onChange={(event) => handleSlideChange(event, id)}></input>
-        <div className="goalName">
-        <i className="fas fa-atom"></i><h2>{name}</h2>
+        <div className="goalElementsContainer">
+          <div className="goalName">
+          <i className="fas fa-atom"></i><h2>{name}</h2>
+          </div>
+          <div className="goalDetails">
+              <h2>{done}</h2>
+              <p>/ {goalValue} {measuring}</p>
+          </div>
+          <button type="button" onClick={() => handleDelete(id)}>
+            <i className="fa fa-trash"></i>
+          </button>
         </div>
-        <div className="goalDetails">
-            <h2>{done}</h2>
-            <p>/ {goalValue} {measuring}</p>
-        </div>
-        <button type="button" onClick={() => handleDelete(id)}>
-          <i className="fa fa-trash"></i>
-        </button>
     </div>
   );
 };
