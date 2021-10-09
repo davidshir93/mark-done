6a733d688941d4d2bebcdc55e62edf2eb274a390
David Shir <davidshir93@gmail.com>
2021-10-09 19:46:01 +0300

    add new goal form styles

--- src/Components/NewGoalForm/NewGoalForm.js
+++ src/Components/NewGoalForm/NewGoalForm.js
@@ -41,13 +41,27 @@ const NewGoalForm = ({ addNewGoal }) => {
 
     return (
         <form onSubmit={handleSubmit}>
+            <label>Goal Name:</label>
             <input type="text" name={name} value={name} onChange={handleNameChange} placeholder="Goal Name"/>
-            <input type="text" name={icon} value={icon} onChange={handleIconChange} placeholder="Choose Icon"/>
-            <input type="text" name={measurement} value={measurement} onChange={handleMeasurementChange} placeholder="Choose Mesurement Unit"/>
-            <input type="text" name={goalValue} value={goalValue} onChange={handleGoalValueChange} placeholder="Choose Goal Value"/>
-            <input type="text" name={goalFrequency} value={goalFrequency} onChange={handleGoalFrequencyChange} placeholder="Choose Goal Frequency"/>
-            <input type="text" name={color} value={color} onChange={handleColorChange} placeholder="Choose Color"/>
-            <input type="submit" name="Submit" />
+            <div className="secRowLabels">
+                <label className="do">Do:</label>
+                <label className="per">Per:</label>
+            </div>    
+            <div className="secRow">
+                <input type="text" name={goalValue} value={goalValue} onChange={handleGoalValueChange} placeholder="Choose Goal Value"/>
+                <input type="text" name={measurement} value={measurement} onChange={handleMeasurementChange} placeholder="Choose Mesurement Unit"/>
+                <input type="text" name={goalFrequency} value={goalFrequency} onChange={handleGoalFrequencyChange} placeholder="Choose Frequency"/>
+            </div>
+            <div className="thirdRowLabels">
+                <label className="icon">Icon:</label>
+                <label className="color">Color:</label>
+            </div>    
+            <div className="thirdRow">
+                <input type="text" name={icon} value={icon} onChange={handleIconChange} placeholder="Choose Icon"/>
+                <input type="text" name={color} value={color} onChange={handleColorChange} placeholder="Choose Color"/>
+            </div>
+            <input type="submit" name="Add" />
+            <p>Cancel</p>
         </form>
     )
 }
