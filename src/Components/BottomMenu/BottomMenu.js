17475ab6a7efa00e7553e07695027cea89e446ee
David Shir <davidshir93@gmail.com>
2022-01-08 17:15:17 +0200

    css updates

--- src/Components/BottomMenu/BottomMenu.js
+++ src/Components/BottomMenu/BottomMenu.js
@@ -5,16 +5,14 @@ const BottomMenu = ({ toggleShowForm, resetToInitialState }) => {
 	return (
 		<div>
 			<div className="bottom-menu-container">
-				<div
-					onClick={toggleShowForm}
-					className="fas fa-plus-circle"
-					title="Add a New Goal"
-				/>
-				<div
-					onClick={resetToInitialState}
-					className="fas fa-undo-alt"
-					title="Reset to Initial State"
-				/>
+				<div className="text-and-icon-container" onClick={toggleShowForm}>
+					<div className="fas fa-plus-circle" title="Add a New Goal" />
+					<div className="button-text">Add a New Goal</div>
+				</div>
+				<div className="text-and-icon-container" onClick={resetToInitialState}>
+					<div className="fas fa-undo-alt" title="Reset to Initial State" />
+					<div className="button-text">Reset to Initial State</div>
+				</div>
 			</div>
 		</div>
 	);
